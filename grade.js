27382d707e061239d9dd70bbe23d888d52775ce7
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-07-13 13:00:39 +0300

    js code calculation

---
+++ grade.js
@@ -0,0 +1,14 @@
+var grade=parseInt(prompt("enter grade"));
+if (grade<=20){
+    alert("E");
+  } else if ((grade>=21)&&(grade<=39)){
+    alert("D");
+  } else if((grade>=40)&&(grade<=59)){
+      alert("c");
+  }
+  else if((grade>=60)&&(grade<=79)){
+    alert("B");
+}
+  else {
+    alert("A");
+  }
